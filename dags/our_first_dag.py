ae3d6766358df300610084027a1f5e36ee74e583
edwin <edwin@DESKTOP-NQDLE5Q>
2024-02-01 11:05:28 +0800

    first dag!

---
+++ dags/our_first_dag.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'edwin',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id='our_first_dag_v5',
+    default_args=default_args,
+    description='This is our first dag that we write',
+    start_date=datetime(2024, 1, 30, 15),
+    schedule_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command="echo hello world, this is the first task!"
+    )
+
+    task2 = BashOperator(
+        task_id='second_task',
+        bash_command="echo hey, I am task2 and will be running after task1!"
+    )
+
+    task3 = BashOperator(
+        task_id='third_task',
+        bash_command="echo hey, I am task3 and will be running after task1 at the same time as task2!"
+    )
+
+    # Task dependency method 1
+    # task1.set_downstream(task2)
+    # task1.set_downstream(task3)
+
+    # Task dependency method 2
+    # task1 >> task2
+    # task1 >> task3
+
+    # Task dependency method 3
+    task1 >> [task2, task3]
